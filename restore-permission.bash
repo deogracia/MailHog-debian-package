f6af8aeee60073b294b002d49d86902fc9384e8c
Lionel Félicité <deogracia@free.fr>
2015-04-03 18:56:13 +0000

    still in dirty mode ...

---
+++ restore-permission.bash
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# still a temporary solution till I use the upstream source
+# to create my deb package
+
+chmod 644 package/DEBIAN/conffiles
+chmod 755 package/DEBIAN/postinst
+chmod 755 package/DEBIAN/postrm
+chmod 755 package/DEBIAN/preinst
+chmod 755 package/etc/
+chmod 755 package/etc/default/
+chmod 644 package/etc/default/mailhog
+chmod 755 package/etc/init.d/
+chmod 755 package/etc/init.d/mailhog
+chmod 755 package/etc/init.d/mailhog
+chmod 755 package/usr/
+chmod 755 package/usr/share/
+chmod 755 package/usr/share/doc/
+chmod 755 package/usr/share/doc/mailhog/
+chmod 644 package/usr/share/doc/mailhog/LICENSE.md
+chmod 644 package/usr/share/doc/mailhog/changelog.Debian.gz
+chmod 644 package/usr/share/doc/mailhog/copyright
+chmod 755 package/usr/share/lintian/
+chmod 755 package/usr/share/lintian/overrides/
+chmod 644 package/usr/share/lintian/overrides/mailhog
+chmod 755 package/usr/share/man/
+chmod 755 package/usr/share/man/man8/
+chmod 644 package/usr/share/man/man8/mailhog.8.gz
+
